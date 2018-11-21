eb66c55ca5da723e7a72c048e156771807f382df
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2018-11-21 12:54:32 +0000

    Running functional tests locally and via command line

---
+++ functional-test/out/test/resources/org/gt/shipping/book/book.feature
@@ -0,0 +1,7 @@
+Feature: Book a Cargo
+
+  Scenario: Successful booking of Cargo
+    Given a customer intends to book a cargo
+    When the customer attempts to book a cargo
+    Then the cargo should be booked
+    And the customer should provided a tracking Id
---
+++ functional-test/src/test/java/org/gt/shipping/book/BookingStepDefs.java
@@ -0,0 +1,18 @@
+package org.gt.shipping.book;
+
+import cucumber.api.PendingException;
+import cucumber.api.java8.En;
+
+public class BookingStepDefs implements En {
+    public BookingStepDefs() {
+        Given("^a customer intends to book a cargo$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+
+        When("^the customer attempts to book a cargo$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+    }
+}
