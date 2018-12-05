cfde4929f71134897b2ea61cbebae7c73fc6ed19
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2018-12-05 23:36:53 +0000

    Added missing actuator dependency

--- functional-test/src/test/java/org/gt/shipping/service/CustomerServiceClient.java
+++ functional-test/src/test/java/org/gt/shipping/service/CustomerServiceClient.java
@@ -1,17 +1,8 @@
 package org.gt.shipping.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-
-import java.util.Map;
-import java.util.Objects;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Scope("cucumber-glue")
 @Controller
